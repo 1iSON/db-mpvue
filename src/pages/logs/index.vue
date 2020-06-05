4f5d41bec59cb788007919fa4c0f661a0f4889d1
1iSON <13692176069@163.com>
2020-06-05 16:01:51 +0800

    修正已知问题

--- src/pages/logs/index.vue
+++ src/pages/logs/index.vue
@@ -1,24 +1,18 @@
 <template>
   <div>
+    <open-data class="infoImg" type="userAvatarUrl"></open-data>
     <div class="user-info">
-      <div class="user-avatar">
-        <open-data type="userAvatarUrl"></open-data>
-      </div>
-      <div class="user-area">
-        <open-data type="userNickName"></open-data>
-        <open-data type="userGender" lang="zh_CN"></open-data>
-        <open-data type="userCountry" lang="zh_CN"></open-data>-
-        <open-data type="userProvince" lang="zh_CN"></open-data>-
-        <open-data type="userCity" lang="zh_CN"></open-data>
-      </div>
+      <open-data class="userinfo-nickname" type="userNickName"></open-data>
+      <open-data type="userGender" lang="zh_CN"></open-data>
+      <open-data type="userCountry" lang="zh_CN"></open-data>-
+      <open-data type="userProvince" lang="zh_CN"></open-data>-
+      <open-data type="userCity" lang="zh_CN"></open-data>
     </div>
-    <div class="user-logs">
-      <ul class="container log-list">
-        <li v-for="(log, index) in logs" :class="{ red: aa }" :key="index" class="log-item">
-          <card :text="(index + 1) + ' . ' + log"></card>
-        </li>
-      </ul>
-    </div>
+    <ul class="container log-list">
+      <li v-for="(log, index) in logs" :class="{ red: aa }" :key="index" class="log-item">
+        <card :text="(index + 1) + ' . ' + log"></card>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
@@ -45,19 +39,10 @@ export default {
   }
 }
 </script>
-<style scope>
+<style>
 .user-info{
   display: flex;
   justify-content: center;
-  align-items: center;
-}
-.user-avatar{
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-}
-.user-area{
-  margin-left: 10px;
 }
 image{
   width: 100%;
